0176237713b093fe0819c7047fbb53b481bba0d5
Zhixin.Zhao <064868@600869.com>
2017-04-14 21:09:48 +0800

    fixed

---
+++ src/main/java/com/samzhao/boot/GirlProperties.java
@@ -0,0 +1,38 @@
+package com.samzhao.boot;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by SamZhao on 2017/4/14.
+ */
+@Component
+@ConfigurationProperties(prefix = "girl")
+public class GirlProperties {
+    private String cupSize;
+    private int age;
+
+    public String getCupSize() {
+        return cupSize;
+    }
+
+    public void setCupSize(String cupSize) {
+        this.cupSize = cupSize;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "GirlProperties{" +
+                "cupSize='" + cupSize + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
--- src/main/java/com/samzhao/boot/HelloController.java
+++ src/main/java/com/samzhao/boot/HelloController.java
@@ -1,17 +1,26 @@
 package com.samzhao.boot;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by SamZhao on 2017/4/13.
  */
 @RestController
+@RequestMapping(value = "hello")
 public class HelloController {
 
-    @RequestMapping(value = "/hello", method = RequestMethod.GET)
-    private String sayHello(){
-        return "Hello Spring Boot!";
+    @Autowired
+    private GirlProperties girlProperties;
+
+    @RequestMapping(value = "/hello/{id}", method = RequestMethod.GET)
+    private String sayHello(@PathVariable("id") Integer id) {
+        return "id: " + id;
+    }
+
+//    @RequestMapping(value = "/hi", method = RequestMethod.GET)
+    @GetMapping(value = "/hi")
+    private String hi(@RequestParam( value = "id" ,required = false , defaultValue = "66666") Integer id) {
+        return "id: " + id;
     }
 }
